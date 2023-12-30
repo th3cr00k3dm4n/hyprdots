b0618b77cde5eba2a84b6b2ccedad57f09e8268c
th3cr00k3dm4n <nandudreamer69@gmail.com>
2023-12-30 11:09:11 +0530

    initial commit

--- .config/micro/backups/%home%cr00k3d%.scripts%weather.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-API_KEY="your_openweathermap_api_key"
-CITY="your_city"
-UNITS="metric" # You can change this to imperial for Fahrenheit
-
-weather=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=$CITY&units=$UNITS&appid=$API_KEY")
-
-echo $weather
--- .scripts/show_brightness.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send -t 700 "Brightness: " -h int:value:"`brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}'`"
